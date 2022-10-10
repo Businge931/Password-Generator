b55e27098b7bd93f14770750578e4b19eb21fb7d
Businge931 <jimmybusinge9@gmail.com>
2022-10-10 11:56:28 +0300

    Optimizing the generate password function

--- src/utils.js
+++ src/utils.js
@@ -6,8 +6,8 @@ const regexForUppercase = /[A-Z]/g;
 const regexForNumbers = /[0-9]/g;
 const regexForSymbols = /[^a-zA-Z0-9 ]/g;
 
-const withoutUppercaseLetters = chars.match(regexForLowercase);
-const withoutLowercaseLetters = chars.match(regexForUppercase);
+// const withoutUppercaseLetters = chars.match(regexForLowercase);
+// const withoutLowercaseLetters = chars.match(regexForUppercase);
 const withNumbers = chars.match(regexForNumbers);
 const withSymbols = chars.match(regexForSymbols);
 const withUppercaseLetters = chars.match(regexForUppercase);
@@ -23,133 +23,23 @@ const randomPassGenerator = (length, chars) => {
 };
 
 export const generateRandomPassword = (values, passwordLength) => {
-  let newCharSet = chars;
-  if (
-    values?.hasUpperCase &&
-    values?.hasLowerCase &&
-    values?.hasNumbers &&
-    values?.hasSymbols
-  ) {
-    newCharSet = chars;
-  } else {
-    if (
-      !values?.hasUpperCase &&
-      values?.hasLowerCase &&
-      values?.hasNumbers &&
-      values?.hasSymbols
-    ) {
-      newCharSet = withoutUppercaseLetters.concat(withNumbers, withSymbols);
-    } else if (
-      values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      values?.hasNumbers &&
-      values?.hasSymbols
-    ) {
-      newCharSet = withoutLowercaseLetters.concat(withNumbers, withSymbols);
-    } else if (
-      values?.hasUpperCase &&
-      values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      values?.hasSymbols
-    ) {
-      newCharSet = withUppercaseLetters.concat(
-        withLowercaseLetters,
-        withSymbols
-      );
-    } else if (
-      values?.hasUpperCase &&
-      values?.hasLowerCase &&
-      values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      newCharSet = withUppercaseLetters.concat(
-        withNumbers,
-        withLowercaseLetters
-      );
-    }
+  let newCharSet = "";
+  const mapTOflags = {
+    hasUpperCase: withUppercaseLetters.join(""),
+    hasLowerCase: withLowercaseLetters.join(""),
+    hasNumbers: withNumbers.join(""),
+    hasSymbols: withSymbols.join(""),
+  };
 
-    //TWO CHECKED BOXES
-    else if (
-      !values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      values?.hasNumbers &&
-      values?.hasSymbols
-    ) {
-      newCharSet = withNumbers.concat(withSymbols);
-    } else if (
-      !values?.hasUpperCase &&
-      values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      values?.hasSymbols
-    ) {
-      newCharSet = withLowercaseLetters.concat(withSymbols);
-    } else if (
-      !values?.hasUpperCase &&
-      values?.hasLowerCase &&
-      values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      newCharSet = withNumbers.concat(withLowercaseLetters);
-    } else if (
-      values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      values?.hasSymbols
-    ) {
-      newCharSet = withUppercaseLetters.concat(withSymbols);
-    } else if (
-      values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      newCharSet = withLowercaseLetters.concat(withSymbols);
-    } else if (
-      values?.hasUpperCase &&
-      values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      newCharSet = withUppercaseLetters.concat(withLowercaseLetters);
+  Object.keys(values).forEach((key) => {
+    // console.log(values[key]);
+    if (values[key] && mapTOflags[key]) {
+      newCharSet += mapTOflags[key];
+      return newCharSet;
+    } else {
+      return newCharSet;
     }
-    //ONLY ONE CHECKED
-    else if (
-      values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      newCharSet = withUppercaseLetters;
-    } else if (
-      !values?.hasUpperCase &&
-      values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      newCharSet = withLowercaseLetters;
-    } else if (
-      !values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      newCharSet = withNumbers;
-    } else if (
-      !values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      values?.hasSymbols
-    ) {
-      newCharSet = withSymbols;
-    } else if (
-      !values?.hasUpperCase &&
-      !values?.hasLowerCase &&
-      !values?.hasNumbers &&
-      !values?.hasSymbols
-    ) {
-      return;
-    }
-  }
-
+  });
+  console.log("char set", newCharSet);
   return randomPassGenerator(passwordLength, newCharSet);
 };
