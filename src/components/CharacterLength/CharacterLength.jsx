d812ead1222db25cdf653e9f0227c88afc6a9701
Businge931 <jimmybusinge9@gmail.com>
2022-10-20 13:24:54 +0300

    slider

--- src/components/CharacterLength/CharacterLength.jsx
+++ src/components/CharacterLength/CharacterLength.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useRef } from "react";
+import { useRef } from "react";
 import classes from "./CharacterLength.module.css";
 
 const CharacterLength = ({ passwordLength, setPasswordLength }) => {
@@ -19,22 +19,22 @@ const CharacterLength = ({ passwordLength, setPasswordLength }) => {
         <p className={classes.length_word}>Character Length</p>
         <p className={classes.length_number}>{passwordLength}</p>
       </div>
-      <div className={classes.range_outer}>
-        {/* <span
+      {/* <div className={classes.range_outer}> */}
+      {/* <span
           className={classes.range_span}
           style={{ width: `${spn}px` }}
         ></span> */}
-        <input
-          type="range"
-          min="2"
-          max="10"
-          step="1"
-          ref={inputRef}
-          defaultValue={6}
-          onChange={manageLength}
-          className={classes.range_slider}
-        />
-      </div>
+      <input
+        type="range"
+        min="2"
+        max="10"
+        step="1"
+        ref={inputRef}
+        defaultValue={6}
+        onChange={manageLength}
+        className={classes.range_slider}
+      />
+      {/* </div> */}
     </>
   );
 };
