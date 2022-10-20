0be2a77b7d0b9594fee28789d984bd41974f5787
Businge931 <jimmybusinge9@gmail.com>
2022-10-20 13:06:38 +0300

    changing color

--- src/components/CharacterLength/CharacterLength.jsx
+++ src/components/CharacterLength/CharacterLength.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useState, useRef } from "react";
 import classes from "./CharacterLength.module.css";
 
 const CharacterLength = ({ passwordLength, setPasswordLength }) => {
+  const [spn, setSpn] = useState();
+
+  const inputRef = useRef(null);
+
   const manageLength = (e) => {
     setPasswordLength(e.target.value);
+    console.log(e);
+    // setSpn(e.target.value);
   };
 
   return (
@@ -12,15 +19,22 @@ const CharacterLength = ({ passwordLength, setPasswordLength }) => {
         <p className={classes.length_word}>Character Length</p>
         <p className={classes.length_number}>{passwordLength}</p>
       </div>
-      <input
-        type="range"
-        min="2"
-        max="10"
-        step="2"
-        defaultValue={5}
-        onChange={manageLength}
-        className={classes.range}
-      />
+      <div className={classes.range_outer}>
+        {/* <span
+          className={classes.range_span}
+          style={{ width: `${spn}px` }}
+        ></span> */}
+        <input
+          type="range"
+          min="2"
+          max="10"
+          step="1"
+          ref={inputRef}
+          defaultValue={6}
+          onChange={manageLength}
+          className={classes.range_slider}
+        />
+      </div>
     </>
   );
 };
